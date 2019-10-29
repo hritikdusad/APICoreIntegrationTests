c66b95af5e253fc1353136873b2937a21489798e
Jonathan Danylko <jdanylko@gmail.com>
2019-10-28 22:16:38 -0400

    Forgot Integration tests

---
+++ ApiCoreIntegrationTest.Tests/Infrastructure/TestHost.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using APIIntegrationDemo;
+using APIIntegrationDemo.Context;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ApiCoreIntegrationTest.Tests.Infrastructure
+{
+    public class TestHost<TStartup> : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Create a new service provider.
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                // Add a database context (AppDbContext) using an in-memory database for testing.
+                services.AddDbContext<DisneyContext>(options =>
+                {
+                    options.UseInMemoryDatabase("IntegrationTests");
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+                // Build the service provider.
+                var sp = services.BuildServiceProvider();
+
+                // Create a scope to obtain a reference to the database contexts
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var context = scopedServices.GetRequiredService<DisneyContext>();
+
+                    var logger = scopedServices.GetRequiredService<ILogger<TestHost<TStartup>>>();
+
+                    // Ensure the database is created.
+                    context.Database.EnsureCreated();
+
+                    try
+                    {
+                        // Seed your [fake] database with some specific test data.
+                        context.SeedFakeDatabase();
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "An error occurred seeding the " +
+                                            "database with test messages. Error: {ex.Message}");
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ ApiCoreIntegrationTest.Tests/IntegrationTests/AttractionApiTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ApiCoreIntegrationTest.Tests.Infrastructure;
+using APIIntegrationDemo;
+using APIIntegrationDemo.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace ApiCoreIntegrationTest.Tests.IntegrationTests
+{
+    [TestClass]
+    public class AttractionApiTest
+    {
+        private TestHost<Startup> _server;
+        private HttpClient _client;
+
+        [TestInitialize]
+        public void AttractionApiTestSetup()
+        {
+            _server = new TestHost<Startup>();
+            _client = _server.CreateClient();
+        }
+
+        [TestCategory("Integration")]
+        [TestMethod]
+        public async Task GetTwoAttractionsTest()
+        {
+            // Arrange
+            var httpResponse = await _client.GetAsync("/api/Attraction");
+
+            // MUST be successful.
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Act
+            var stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            var attractions = JsonConvert.DeserializeObject<List<Attraction>>(stringResponse);
+
+            // Assert
+            Assert.IsNotNull(attractions);
+            Assert.IsTrue(attractions.Count == 2);
+        }
+
+    }
+}
